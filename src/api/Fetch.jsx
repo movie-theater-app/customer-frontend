f05af2858f578a4f26088b071c1dba792248bca3
Anniina-55 <anniina.leikas55@gmail.com>
2025-11-09 20:43:29 +0200

    Deployed backend address as comment

--- src/api/Fetch.jsx
+++ src/api/Fetch.jsx
@@ -1,5 +1,6 @@
-// this was previously /api but when i tested locally just "/" was working
-const API_BASE_URL = 'https://demo-northstar-movie-theatre.azurewebsites.net'; 
+// deployed backend address: https://demo-northstar-movie-theatre.azurewebsites.net
+// local address since database is not yet deployed
+const API_BASE_URL = '/api'; 
 
 // Theaters
 export const theaterApi = {
