b522e4c19255e6ae8b8238765cc583428d8adc40
Anniina-55 <anniina.leikas55@gmail.com>
2025-11-19 17:39:33 +0200

    Deleted test views

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import './App.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Main from './views/Main'
-import MoviePage from './views/MoviePage';
 import SelectSeats from './views/SelectSeats';
-import MovieDetailsPage from './views/MovieDetailsPage';
 
 const router = createBrowserRouter([
   {
@@ -11,17 +9,10 @@ const router = createBrowserRouter([
     element: <Main />
   },
   {
-    path: "/movie",
-    element: <MoviePage/>
-  },
-   {
     path: "/seat-map/:auditoriumId", 
     element: <SelectSeats />
   },
-  {
-    path: "/movie/:movieId",
-    element:  <MovieDetailsPage />
-  }
+ 
 
 ]);
 
