213365e91fe0b9a5e7a2291099da20109037845e
Anniina-55 <anniina.leikas55@gmail.com>
2025-11-25 19:52:36 +0200

    Changed seat-map to use scheduleId instead of auditoriumId

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const router = createBrowserRouter([
     element: <Movie />
   },
   {
-    path: "/seat-map/:auditoriumId", 
+    path: "/seat-map/:scheduleId", 
     element: <SelectSeats />
   }
  
---
+++ src/api/scheduleApi.jsx
@@ -0,0 +1,12 @@
+const VITE_API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const scheduleApi = {
+
+  // get single schedule based on id
+  getSchedule: async (scheduleId) => {
+    const response = await fetch(`${VITE_API_BASE_URL}/api/schedules/${scheduleId}`);
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+    return await response.json();
+  },
+
+}
