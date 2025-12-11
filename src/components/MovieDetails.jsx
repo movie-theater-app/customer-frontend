d830372d56158d2d76b82618da8028f15ba9d086
Lyren <vaino.s.makinen@gmail.com>
2025-12-11 17:41:28 +0200

    Extra details for movies

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -6,6 +6,20 @@ export default function MovieDetails({ movie }) {
       <div className="movie-poster">
         <img src={movie.poster_url} alt={movie.title} />
       </div>
+      <div className="movie-info">
+        <div className="movie-info-item">
+          <span className="info-header">Genre</span>
+          <span className="info-value">{movie.genre}</span>
+        </div>
+        <div className="movie-info-item">
+          <span className="info-header">Duration</span>
+          <span className="info-value">{movie.duration_minutes} min</span>
+        </div>
+        <div className="movie-info-item">
+          <span className="info-header">Age Rating</span>
+          <span className="info-value">{movie.age_rating}</span>
+        </div>
+      </div>
     </div>
   );
 }
